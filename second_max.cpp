79141214cc6f29245abe6aa7a02da0cb22267049
sankar6305 <58016341+sankar6305@users.noreply.github.com>
2021-05-28 11:39:58 +0530

    Add files via upload

---
+++ second_max.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int arr[]={9,9,9,9,9,9,9,9};
+	int val1=arr[0];
+	int val2=-1;
+	for(int i=1;i<8;i++){
+		if(arr[i]>val1){
+			val2=val1;
+			val1=arr[i];
+		}
+	}
+	cout<<"Second largest element \n"<<val2;
+}
